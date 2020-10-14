35ee9195ad0e2a8d59abc98a4b09e32d79edc6f3
SubhashKV14 <69677080+SubhashKV14@users.noreply.github.com>
2020-10-14 15:37:16 +0530

    Update Gamestate.js

--- Gamestate.js
+++ Gamestate.js
@@ -17,7 +17,7 @@ class State {
   }
 
   async start() {
-    if (gamestate === 0) {
+    if (gameState === 0) {
       player = new Player();
       var playerCountRef = await database.ref('playerCount').once("value");
       if (playerCountRef.exists()) {
@@ -27,5 +27,83 @@ class State {
       form = new Login();
       form.display();
     }
+
+    var resoln = 60;
+    var cols = height / resoln;
+    var rows = width / resoln;
+  
+    var x = 0;
+    var y = (rows - 1) * resoln;
+    var dir = 1;
+    
+  
+    for (var i = 0; i < cols * rows; i++) {
+      var tile = new Tile(x, y, resoln, i, i + 1);
+      tiles.push(tile);
+      x += (resoln * dir);
+      if (x >= width || x <= -resoln) {
+        dir *= -1;
+        x += resoln * dir;
+        y -= resoln;
+      }
+    }
+  
+  //snakes
+    for (var i = 0; i < 5; i++) {
+      var index = floor(random(cols, tiles.length-1));
+      tiles[index].snadder = -1 * floor(random(index % cols, index - 1));
+    }
+  
+  //ladders
+    for (var i = 0; i < 5; i++) {
+      var index = floor(random(0, tiles.length - cols));
+      tiles[index].snadder = floor(random(cols - (index % cols), tiles.length - index - 1));
+    }  
+    //player = new Player();
+  }
+
+  play() {
+    form.hide();
+
+    for (var tile of tiles) {
+      tile.display();
+    }
+    for (var tile of tiles) {
+      tile.showSnadders();
+    }
+  
+    if (state === ROLLSTATE) {
+      player.rollDice();
+      rolls[index]++;
+      player.showPreview();
+      state = MOVESTATE;
+    } else if (state === MOVESTATE) {
+      player.move();
+      if (player.isSnadder()) {
+        state = SNADDER;
+      } else {
+        state = ROLLSTATE;
+      }
+    } else if (state === SNADDER) {
+      player.moveSnadder();
+      state = ROLLSTATE;
+    }
+  
+    var gameover = false;
+    if (player.spot >= tiles.length - 1) {
+      player.spot = tiles.length - 1;
+      gameover = true;
+    }
+    player.display();
+  
+    if (gameover) {
+      player.reset();
+      index++;
+      rolls[index] = 0;
+    }
+  }
+
+  end() {
+    console.log("end");
   }
 }
